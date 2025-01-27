9c29a00cbdec350f16b3b5034ec7232524f608fd
Mando <hey@armandohimself.com>
2025-01-27 05:53:01 -0600

    Function + Alias Types

--- app.js
+++ app.js
@@ -13,3 +13,36 @@ user = {
 console.log(user.name);
 user.name = "Armando Arteaga";
 console.log(user.name);
+//! Arrays
+// long way
+var hobbies;
+//short way
+var movies;
+hobbies = ["Coding", "Cooking", "Working Out"];
+movies = ["Predestination", "Nosferatu", "Iron Man 2"];
+// Types in arguements
+function add(a, b) {
+    // infers the type and enforced in constant
+    var result = a + b;
+    console.log(result);
+}
+// Return Types on Functions
+function subtract(a, b) {
+    // infers the type and enforced in constant
+    var result = a - b;
+    console.log(result);
+    return result;
+}
+// Valid JS code
+function calculate(a, b, calcFn) {
+    // calcFn intended to be used as a callable function passed as an argument is viable but it's of type any right now
+    calcFn(a, b);
+}
+function calculate1(a, b, calcFn) {
+    // subtract(a: number, b: number): number is how you define a type
+    // so you use this syntax to define a callable function type of 2 input parameter both of type number and return type number:
+    // (PARAMTERS + TYPES) => RETURN TYPE
+    calcFn(a, b);
+}
+// now you can call it like this:
+console.log(calculate(5, 4, add));
--- app.ts
+++ app.ts
@@ -33,3 +33,54 @@ let movies: string[];
 
 hobbies = ["Coding", "Cooking", "Working Out"];
 movies = ["Predestination", "Nosferatu", "Iron Man 2"];
+
+// Types in arguements
+function add(a: number, b: number) {
+  // infers the type and enforced in constant
+  const result = a + b;
+  console.log(result);
+}
+
+// Return Types on Functions
+function subtract(a: number, b: number): number {
+  // infers the type and enforced in constant
+  const result = a - b;
+  console.log(result);
+  return result;
+}
+
+// Valid JS code
+function calculate(a: number, b: number, calcFn) {
+  // calcFn intended to be used as a callable function passed as an argument is viable but it's of type any right now
+  calcFn(a, b);
+}
+
+function calculate1(
+  a: number,
+  b: number,
+  calcFn: (a: number, b: number) => number
+) {
+  // subtract(a: number, b: number): number is how you define a type
+  // so you use this syntax to define a callable function type of 2 input parameter both of type number and return type number:
+  // (PARAMTERS + TYPES) => RETURN TYPE
+  calcFn(a, b);
+}
+
+// now you can call it like this:
+console.log(calculate(5, 4, add));
+
+// You can create "custom types or aliases" using the type keyword
+type AddFn = (a: number, b: number) => number;
+// even though we have an = sign, because we use type, we're not assigning, we are creating a type!
+// Now use it in the function above to replace calcFn to make it simpler
+
+// Other types
+type StringOrNum = string | number;
+
+// object alias types
+type Person = {
+  name: string;
+  dob: Date;
+};
+
+let person: Person;
