525e0906fa91891afb1139a4c3bb243187cf30e5
Mando <hey@armandohimself.com>
2025-01-27 05:28:55 -0600

    Create Object Type

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 var myName = "Armando";
+var userId = "abc123";
+// Is how we define an object type
+// let user: {};
+// Then you can define the shape of what you are looking for in your object. Using ; is more common
+var user;
+// Now we must initialize our user object
+user = {
+    name: "Armando",
+    age: 32,
+    isAdmin: "true",
+};
+console.log(user.name);
+user.name = "Armando Arteaga";
+console.log(user.name);
--- app.ts
+++ app.ts
@@ -1,3 +1,26 @@
 let myName: string = "Armando";
 
 let userId: string | number = "abc123";
+
+// Is how we define an object type
+// let user: {};
+
+// Then you can define the shape of what you are looking for in your object. Using ; is more common
+let user: {
+  name: string;
+  age: number;
+  isAdmin: string | boolean;
+};
+
+// Now we must initialize our user object
+user = {
+  name: "Armando",
+  age: 32,
+  isAdmin: "true",
+};
+
+console.log(user.name);
+
+user.name = "Armando Arteaga";
+
+console.log(user.name);
